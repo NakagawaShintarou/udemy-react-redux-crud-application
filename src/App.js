2596481f4b4763da64e5f950214d7fbc34155621
NakagawaShintarou <nakagawa.shintaro@tss.co.jp>
2020-06-29 10:36:17 +0900

    jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React,{ Component} from 'react';
 
-function App() {
-  return <h1>Hello,world!</h1>;
-}
-
-//class App extends Component {
-//  render(){
-//      return React.createElement(
-//        "div",
-//        null,
-//        "Hello,world!!!!!"
-//      );
-//  }
+//function App() {
+//  return <h1>Hello,world!</h1>;
 //}
+
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <label htmlFor="bar"> bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked")}} />;
+      </div>
+    )
+  }
+}
 
 export default App;
