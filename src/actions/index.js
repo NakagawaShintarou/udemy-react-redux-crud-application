c4aec6720441ec8c9aadd7b601b5e64485003108
NakagawaShintarou <nakagawa.shintaro@tss.co.jp>
2020-06-29 13:38:28 +0900

    implement action and creator

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'INCREMENT'
+export const INCREMENT = 'DECREMENT'
+
+export const increment = () => ({
+  type: INCREMENT
+})
+
+export const increment = () => ({
+  type: DECREMENT
+})
