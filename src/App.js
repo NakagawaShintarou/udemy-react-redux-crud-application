c77e3ae37f911021384b7209967d24cf4f5288e5
NakagawaShintarou <nakagawa.shintaro@tss.co.jp>
2020-06-29 10:07:20 +0900

    JSX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import React from 'react';
+import React,{ Component} from 'react';
 
 function App() {
-  return <div><h1>Hello,world!</h1></div>;
+  return <h1>Hello,world!</h1>;
 }
+
+//class App extends Component {
+//  render(){
+//      return React.createElement(
+//        "div",
+//        null,
+//        "Hello,world!!!!!"
+//      );
+//  }
+//}
 
 export default App;
